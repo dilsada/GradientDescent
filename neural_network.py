13234fe38116a937cd90c47302d81f8a7a4ed141
dilsada <dilsadakkoyun@gmail.com>
2023-04-20 18:15:51 +0200

    initial commit

---
+++ neural_network.py
@@ -0,0 +1,172 @@
+import numpy as np
+from load_mnist import load_mnist
+
+class NeuralNetworkClassifier:
+
+    def __init__(self, layer_dimensions, activations):
+        np.random.seed(1)
+        self.layers = len(layer_dimensions) - 1 # should be 3
+        self.W = []
+        self.b = []
+        self.activations = activations
+        self.model_state = {"linear_outputs": [], "activation_outputs": []}
+        self.gradients = {"dW": [], "db": []}
+
+        sigma = 0.01
+        for i in range(1, self.layers + 1):
+            weight = np.random.normal(scale=sigma**2, size=(layer_dimensions[i], layer_dimensions[i-1]))
+            #bias = np.zeros((layer_dimensions[i], 1))
+            bias = np.random.randn(layer_dimensions[i], 1)
+            self.W.append(weight)
+            self.b.append(bias)
+        
+        if self.activations[-1] != "softmax":
+            print("Last activation layer should be softmax.")
+    
+    # Activation Functions
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+    
+    @staticmethod
+    def relu(x):
+        return np.maximum(0.0, x)
+    
+    @staticmethod
+    def softmax(x):
+        e_x = np.exp(x - np.max(x))
+        return e_x / np.sum(e_x, axis=0, keepdims=True)
+    
+    # Forward Propagation
+    def linear_forward(self, A_prev, W, b):
+        """ param A_prev: the activation values from the previous layer or input data
+            param W: the weight matrix (size of current layer x previous layer)
+            param b: the bias vector: (size of current layer x 1)
+            return: the input of the activation function
+        """
+        return np.dot(W, A_prev) + b
+
+    def activation_forward(self, x, activation):
+        """ param x: linear outputs (output of the linear_forward)
+            return activation outputs
+        """
+        if activation == "sigmoid":
+            return self.sigmoid(x)
+        elif activation == "relu":
+            return self.relu(x)
+        elif activation == "softmax":
+            return self.softmax(x)
+    
+    def model_forward(self, x):
+        """ param x: the input data
+        """
+        self.model_state["activation_outputs"].append(np.copy(x.T))
+
+        for l in range(self.layers):
+            linear_output = self.linear_forward(self.model_state["activation_outputs"][-1], self.W[l], self.b[l])
+            activation_output = self.activation_forward(linear_output, self.activations[l])
+            self.model_state["linear_outputs"].append(linear_output)
+            self.model_state["activation_outputs"].append(activation_output)
+        
+        return activation_output
+    
+    # Loss Function
+    def compute_loss(self, y):
+        """ param y: expected output
+        """
+        z = self.model_state["linear_outputs"][-1].T
+        if np.max(z) > 0:
+            z -= np.max(z)
+        loss = np.sum(np.log(np.sum(np.exp(z), axis=0)) - np.sum(z[y==1])) / y.shape[1]
+        loss2 = np.linalg.norm(y - self.model_state["activation_outputs"][-1].T)
+        return loss2
+    
+    # Activation Functions Derivatives
+    @staticmethod
+    def sigmoid_backward(x):
+        return np.exp(-x) / ((1 + np.exp(-x)) ** 2)
+    
+    @staticmethod
+    def relu_backward(x):
+        return [0 if i < 0 else 1 for i in x]
+    
+    # Backwards Propogation
+    def linear_backward(self, W, delta):
+        return np.dot(W.T, delta)
+    
+    def activation_backward(self, x, activation): # f'(Z)
+        if activation == "sigmoid":
+            return self.sigmoid_backward(x)
+        elif activation == "relu":
+            return self.relu_backward(x)
+        
+    def model_backward(self, Y):
+        data_points = Y.shape[0]
+        deltas = [None] * self.layers
+        
+        activation_output = self.model_state["activation_outputs"][-1]
+        deltas[-1] = ((Y.T - activation_output))
+
+        for i in reversed(range(self.layers - 1)):
+            deltas[i] = self.linear_backward(self.W[i+1], deltas[i+1]) * self.activation_backward(self.model_state["linear_outputs"][i], self.activations[i])
+        
+        for i, d in enumerate(deltas):
+            self.gradients["dW"].append(np.dot(d, self.model_state["activation_outputs"][i].T) / float(data_points))
+            self.gradients["db"].append(np.dot(d, np.ones((data_points,1))) / float(data_points))
+
+    def update_parameters(self, alpha):
+        for i in range(self.layers):        
+            self.W[i] += alpha * self.gradients["dW"][i]
+            self.b[i] += alpha * self.gradients["db"][i]
+
+    def predict(self, x):
+        return self.model_forward(x)
+
+    def random_mini_batches(self, x, y, batch_size = 64, use_mini_batch = False):
+        if use_mini_batch == False:
+            return [x], [y]
+        x_mini = []
+        y_mini = []
+        i = 0
+        while i < y.shape[1]:
+            x_mini.append(x[:, i:i+batch_size])
+            y_mini.append(y[:, i:i+batch_size])
+            i += batch_size
+        return x_mini, y_mini
+
+    def train_model(self, x, y, iterations = 1, alpha = 0.01, batch_size = 64):
+        losses = []
+        
+        for i in range(iterations):
+            self.model_forward(x)
+            self.model_backward(y)
+            self.update_parameters(alpha)
+            losses.append(self.compute_loss(y))
+            print("Loss \t{}: {}".format(i, losses[-1]))
+        return losses
+    
+    def predict_accuracy(self, x, y):
+        y_pred = self.predict(x)
+        y_pred_argmax = np.argmax(y_pred, axis=0)
+        success_rate = y_pred_argmax == np.argmax(y, axis=1)
+        success_rate = success_rate.sum() / len(success_rate)
+        return success_rate
+    
+
+
+def main():
+    X_train, Y_train, X_test, Y_test = load_mnist()
+    print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
+
+    layer_dimensions = [X_train.shape[1], 512, 128, Y_train.shape[1]]
+    activations = ["sigmoid", "sigmoid", "softmax"]
+
+    model = NeuralNetworkClassifier(layer_dimensions, activations)
+
+    model.train_model(X_train, Y_train, 35, 0.01, 64)
+
+    print(model.predict_accuracy(X_test, Y_test))
+
+
+if __name__ == "__main__":
+    main()
